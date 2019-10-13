9ee33d49bcec9a40217f98187cdc752f75bf180d
Sergey Kuznetsov <sergey@iterudit.com>
2019-10-12 23:18:15 -0400

    Refactoring and added support of scopes and variables for further mutation analysis

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ if (!process.env.NODE_CONFIG_DIR) {
 
 const config = require('config');
 const fs = require('fs');
-const parser = require('babylon');
+// const parser = require('babylon');
+const parser = require('@babel/parser');
 const _ = require('lodash');
 const request = require('request');
 const SourceMapConsumer = require('source-map').SourceMapConsumer;
 const CodeGenerator = require('./lib/jsgen').CodeGenerator;
+// const generate = require('@babel/generator').default;
 const traverse = require("./lib/traverser");
 
 // Node mutators
@@ -21,11 +23,11 @@ const assignValuesToVariables = require("./lib/mutators/assign_values_to_variabl
 const createScopes = require("./lib/mutators/create_scopes");
 const replaceSequentialAssignmentsInFlowControl = require("./lib/mutators/replace_sequential_assignments_in_flow_control");
 const replaceSequentialAssignments = require("./lib/mutators/replace_sequential_assignments");
-// const recoverNamesFromSourceMaps = require("./lib/mutators/recover_names_from_source_maps");
 const fixControlFlowStatementsWithOneStatement = require("./lib/mutators/fix_control_flow_statements_with_one_statement");
 const removeLocationInformation = require("./lib/mutators/remove_location_information");
 const recoverBooleans = require("./lib/mutators/recover_booleans");
-const { createAllFoldersInPath} = require("./lib/utils");
+const removeParentNodes = require("./lib/mutators/remove_parent_nodes");
+const Utils = require("./lib/utils");
 
 if (typeof(config.verbose) === "undefined" ) {
     console.error(`ERROR: Cannot find config file at ${process.env.NODE_CONFIG_DIR}`);
@@ -40,45 +42,45 @@ const processingFileName = fs.realpathSync(process.argv[2] || config.defaultFile
 const code = fs.readFileSync(processingFileName, "utf8");
 console.log(`Processing ${processingFileName}...`);
 
-let mapUrl = (code.match(/^\/\/#\s*sourceMappingURL=(.+)$/gim)||[""])[0].replace(/^\/\/#\s*sourceMappingURL=/, '');
+// let mapUrl = (code.match(/^\/\/#\s*sourceMappingURL=(.+)$/gim)||[""])[0].replace(/^\/\/#\s*sourceMappingURL=/, '');
 
-new Promise((resolve, reject) => {
-    if (config.sourceMaps.use && mapUrl) {
-        try {
-            request.get(mapUrl, function (err, response, body) {
-                if (err)
-                {
-                    resolve(null);
-                    return;
-                }
+// new Promise((resolve, reject) => {
+//     if (config.sourceMaps.use && mapUrl) {
+//         try {
+//             request.get(mapUrl, function (err, response, body) {
+//                 if (err)
+//                 {
+//                     resolve(null);
+//                     return;
+//                 }
 
-                let smc = new SourceMapConsumer(JSON.parse(body));
-                if (config.sourceMaps.saveOriginals && smc.sources && smc.sources.length > 0) {
-                    let mainPath = process.argc > 2 ? process.argv[3] : config.sourceMaps.defaultOutputFolder;
-                    mainPath = fs.realpathSync(mainPath);
-                    mainPath += (mainPath.last() == "/" ? "" : "/");
-                    createAllFoldersInPath(mainPath);
-                    _.each(smc.sources, (src, idx) => {
-                        let fileName = mainPath +
-                            (mainPath.last() == '/'
-                                || src[0] == '/' ? "" : "/")
-                            + src;
-                        createAllFoldersInPath(fileName);
-                        fs.writeFileSync(fileName, smc.sourcesContent[idx], { flag: "w" });
-                    });
-                }
-                resolve(smc);
-            });
-        } catch (ex) {
-            resolve(null);
-        }    
-    } else {
-        resolve(null);
-    }
-}).then (smc => {
+//                 let smc = new SourceMapConsumer(JSON.parse(body));
+//                 if (config.sourceMaps.saveOriginals && smc.sources && smc.sources.length > 0) {
+//                     let mainPath = process.argc > 2 ? process.argv[3] : config.sourceMaps.defaultOutputFolder;
+//                     mainPath = fs.realpathSync(mainPath);
+//                     mainPath += (mainPath.last() == "/" ? "" : "/");
+//                     Utils.createAllFoldersInPath(mainPath);
+//                     _.each(smc.sources, (src, idx) => {
+//                         let fileName = mainPath +
+//                             (mainPath.last() == '/'
+//                                 || src[0] == '/' ? "" : "/")
+//                             + src;
+//                         Utils.createAllFoldersInPath(fileName);
+//                         fs.writeFileSync(fileName, smc.sourcesContent[idx], { flag: "w" });
+//                     });
+//                 }
+//                 resolve(smc);
+//             });
+//         } catch (ex) {
+//             resolve(null);
+//         }    
+//     } else {
+//         resolve(null);
+//     }
+// }).then (smc => {
     const ast = parser.parse(code, config.parser);
 
-    fs.writeFileSync('./ast.before.json', JSON.stringify(ast, null, 2));
+    // fs.writeFileSync('./ast.before.json', JSON.stringify(ast, null, 2));
 
     const jsGen = new CodeGenerator(ast, config.codeGenerator, "");
 
@@ -90,41 +92,44 @@ new Promise((resolve, reject) => {
             recoverBooleans,
         ],
         {
-            generator: jsGen,
-            // sourceMapConsumer: smc,
             config
         });
 
-    // Step 1
-    while(traverse(
-        ast,
-        [
-            fixControlFlowStatementsWithOneStatement,
-            replaceSequentialAssignments,
-            replaceSequentialAssignmentsInFlowControl
- ],
-        {
-            generator: jsGen,
-            // sourceMapConsumer: smc,
-            config
-        }));
+    while(true) {
+        if(!traverse(
+            ast,
+            [
+                fixControlFlowStatementsWithOneStatement,
+                removeLocationInformation,
+                replaceSequentialAssignments,
+                removeLocationInformation,
+                replaceSequentialAssignmentsInFlowControl,
+                removeLocationInformation,
+            ],
+            {
+                config
+            }
+        )) {
+            console.log('Traversing completed.');
+            break;
+        }
+        console.log('Traversing...');
+    }
 
     // Painting of variables
     traverse(
         ast,
         [
             createScopes,
-            assignValuesToVariables
-        ], {config});
-    
-    // Dumping scopes of variables
-    traverse(
-        ast,
-        [
-            dumpScopes
+            assignValuesToVariables,
         ], {config});
 
-    fs.writeFileSync('./ast.after.json', JSON.stringify(ast, (k,v) => ['parentNode', 'parentNodeProperty'].includes(k) ? null : v, 2));
+    traverse(ast, removeLocationInformation, {config});
+
+    // Dumping scopes of variables
+    traverse(ast,dumpScopes, {config});
+
+    // fs.writeFileSync('./ast.after.json', JSON.stringify(ast, (k,v) => ['parentNode', 'parentNodeProperty'].includes(k) ? null : v, 2));
 
     if (config.verbose) {
         console.log("Traversal finished.");
@@ -132,12 +137,13 @@ new Promise((resolve, reject) => {
     
     const outputFilePath = `${processingFileName}.out`;
 
-    createAllFoldersInPath(outputFilePath);
+    Utils.createAllFoldersInPath(outputFilePath);
 
     let res = null;
     try {
         console.log("Generating code...")    
         res = jsGen.generate();
+        // res = generate(ast, {});
     } catch(ex) {
         console.log("ERROR:", ex.stack);
     }
@@ -154,6 +160,6 @@ new Promise((resolve, reject) => {
             process.exit(0);
         });
     }
-}).catch( ex => {
-    console.log("ERROR:", ex.stack);
-});
+// }).catch( ex => {
+//     console.log("ERROR:", ex.stack);
+// });
